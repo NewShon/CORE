5c8d2396750bc527e9e33500a3278d0574b49e15
Kiryl Navakshonau <Kiryl_Navakshonau@epam.com>
2019-03-06 17:52:21 +0300

    Add project files.

---
+++ CORE.WEB/Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CORE.WEB.Controllers
+{
+	[Route("api/[controller]")]
+	public class ValuesController : Controller
+	{
+		private static List<string> _storage = new List<string>();
+
+		// GET api/values
+		[HttpGet]
+		public IEnumerable<string> Get()
+		{
+			return _storage;
+		}
+
+		// GET api/values/5
+		[HttpGet("{id}")]
+		public string Get(int id)
+		{
+			return _storage[id];
+		}
+
+		// PUT api/values
+		[HttpPut]
+		public void Put(string str)
+		{
+			_storage.Add(str);
+		}
+	}
+}
